9af212ff8990b43f7697fa64266670d6a634d0cb
dcDalin <mcdalinoluoch@gmail.com>
2018-04-21 14:33:17 +0300

    [Chore] :hammer: Initial boilerplate setup

---
+++ project/tests/test_config.py
@@ -0,0 +1,49 @@
+# project/tests/test_config.py
+"""Test app configurations"""
+
+import unittest
+
+from flask import current_app
+from flask_testing import TestCase
+
+from project.server import app
+
+
+class TestDevelopmentConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('project.server.config.DevelopmentConfig')
+        return app
+
+    def test_app_is_development(self):
+        self.assertFalse(app.config['SECRET_KEY'] is 'my_precious')
+        self.assertTrue(app.config['DEBUG'] is True)
+        self.assertFalse(current_app is None)
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] == 'postgresql://postgres:password@localhost/python-flask-api-boilerplate'
+        )
+
+
+class TestTestingConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('project.server.config.TestingConfig')
+        return app
+
+    def test_app_is_testing(self):
+        self.assertFalse(app.config['SECRET_KEY'] is 'my_precious')
+        self.assertTrue(app.config['DEBUG'])
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] == 'postgresql://postgres:password@localhost/python-flask-api-boilerplate_test'
+        )
+
+
+class TestProductionConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('project.server.config.ProductionConfig')
+        return app
+
+    def test_app_is_production(self):
+        self.assertTrue(app.config['DEBUG'] is False)
+
+
+if __name__ == '__main__':
+    unittest.main()
