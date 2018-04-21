8b338a84cf9267e6e0480d6fc7471d39843875ac
dcDalin <mcdalinoluoch@gmail.com>
2018-04-21 18:13:06 +0300

    [Bug] :bug: Fix base test config bug

--- project/tests/test_config.py
+++ project/tests/test_config.py
@@ -6,43 +6,60 @@ import unittest
 from flask import current_app
 from flask_testing import TestCase
 
-from project.server import app
+from project.server import APP
+
+POSTGRES_LOCAL_BASE = 'postgresql://postgres:password@localhost/'
+DATABASE_NAME = 'python_flask_api_boilerplate'
 
 
 class TestDevelopmentConfig(TestCase):
+    """Testing App Development configuration"""
+
     def create_app(self):
-        app.config.from_object('project.server.config.DevelopmentConfig')
-        return app
+        """Get dev mode"""
+        APP.config.from_object('project.server.config.DevelopmentConfig')
+        return APP
 
     def test_app_is_development(self):
-        self.assertFalse(app.config['SECRET_KEY'] is 'my_precious')
-        self.assertTrue(app.config['DEBUG'] is True)
+        """Dev assertions method"""
+        self.assertFalse(APP.config['SECRET_KEY'] is 'my_precious')
+        self.assertTrue(APP.config['DEBUG'] is True)
         self.assertFalse(current_app is None)
         self.assertTrue(
-            app.config['SQLALCHEMY_DATABASE_URI'] == 'postgresql://postgres:password@localhost/python_flask_api_boilerplate'
+            APP.config['SQLALCHEMY_DATABASE_URI'] ==
+            POSTGRES_LOCAL_BASE + DATABASE_NAME
         )
 
 
 class TestTestingConfig(TestCase):
+    """Testing App Test Configuration"""
+
     def create_app(self):
-        app.config.from_object('project.server.config.TestingConfig')
-        return app
+        """Get app test config"""
+        APP.config.from_object('project.server.config.TestingConfig')
+        return APP
 
     def test_app_is_testing(self):
-        self.assertFalse(app.config['SECRET_KEY'] is 'my_precious')
-        self.assertTrue(app.config['DEBUG'])
+        """Testing method assertions"""
+        self.assertFalse(APP.config['SECRET_KEY'] is 'my_precious')
+        self.assertTrue(APP.config['DEBUG'])
         self.assertTrue(
-            app.config['SQLALCHEMY_DATABASE_URI'] == 'postgresql://postgres:password@localhost/python_flask_api_boilerplate_test'
+            APP.config['SQLALCHEMY_DATABASE_URI'] ==
+            POSTGRES_LOCAL_BASE + DATABASE_NAME + '_test'
         )
 
 
 class TestProductionConfig(TestCase):
+    """Testing App Production Config"""
+
     def create_app(self):
-        app.config.from_object('project.server.config.ProductionConfig')
-        return app
+        """Get the production config"""
+        APP.config.from_object('project.server.config.ProductionConfig')
+        return APP
 
     def test_app_is_production(self):
-        self.assertTrue(app.config['DEBUG'] is False)
+        """Assert App Debug mode is false"""
+        self.assertTrue(APP.config['DEBUG'] is False)
 
 
 if __name__ == '__main__':
